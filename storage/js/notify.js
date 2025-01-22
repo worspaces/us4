56f97530c631c57f5944bd02bde7bcd7060d1e4e
voucan <vcn4313@gmail.com>
2025-01-22 10:58:55 -0500

    Update notify.js

--- storage/js/notify.js
+++ storage/js/notify.js
@@ -44,6 +44,30 @@ function createNotification(message) {
 
     notification.appendChild(closeButton);
 
+    const topBar = document.createElement('div');
+    topBar.style.position = 'absolute';
+    topBar.style.top = '0';
+    topBar.style.left = '0';
+    topBar.style.height = '4px';
+    topBar.style.backgroundColor = 'gold';
+    topBar.style.width = '100%';
+    topBar.style.transition = 'width 5s linear';
+
+    notification.appendChild(topBar);
+
+    document.body.appendChild(notification);
+
+    setTimeout(() => {
+        topBar.style.width = '0';
+    }, 50);
+
+    setTimeout(() => {
+        if (document.body.contains(notification)) {
+            document.body.removeChild(notification);
+        }
+    }, 5050);
+}
+
 window.onload = () => {
-    createNotification('Welcome to V1.7!');
+    createNotification('Our discord server was raided. If you were banned, you should be unbanned now');
 };
