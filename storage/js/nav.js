ddc832f6edd786d83e8dbc0a7b1f9887ca7528e2
voucan <vcn4313@gmail.com>
2025-02-01 15:37:47 -0500

    Create nav.js

---
+++ storage/js/nav.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navButtons = `
+    <a href="/"><i class="fa fa-home"></i> </a>
+    <a href="/pages/games"><i class="fas fa-gamepad"></i> </a>
+    <a href="/pages/movies"><i class="fa-solid fa-video"></i> </a>
+    <a href="/pages/apps"><i class="fa-sharp fa-solid fa-layer-group"></i> </a>
+    <a href="/pages/prox"><i class="fas fa-globe"></i> </a>
+    <a href="/pages/partners"><i class="fa-solid fa-users"></i> </a>
+    <a href="/pages/extra"><i class="fas fa-plus"></i> </a>
+    <a href="/pages/settings"><i class="fa-solid fa-gear"></i> </a>
+    `;
+
+    document.querySelector('.right-side').innerHTML = navButtons;
+});
