5185aecd6317e3cbdb201431376ea34b8ec99418
voucan <vcn4313@gmail.com>
2025-02-09 16:28:12 -0500

    Update background.js

--- storage/js/background.js
+++ storage/js/background.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function() {
+    document.body.style.backgroundImage = 'none';
+    const storedBackgroundImage = localStorage.getItem('backgroundImage');
+    if (storedBackgroundImage) {
+        document.body.style.backgroundImage = `url("${storedBackgroundImage}")`;
+    }
     document.body.style.backgroundColor = localStorage.getItem('backgroundColor') || '#000';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
 });
