df121623ada6c4f5cee044659880b22f7ae59f5b
voucan <vcn4313@gmail.com>
2025-02-11 17:42:47 -0500

    Update background.js

--- storage/js/background.js
+++ storage/js/background.js
@@ -1,4 +1,16 @@
 window.addEventListener('load', function() {
-    document.body.style.backgroundColor = localStorage.getItem('backgroundColor') || '#000';
-    localStorage.setItem('backgroundColor', '#b61924');
+    const today = new Date();
+    const month = today.getMonth();
+    const date = today.getDate();
+
+    if (month === 1 && date === 14) {
+        document.body.style.backgroundColor = '#FF0000';
+    }
+
+    else if (month === 1 && date >= 15 && date <= 21 && today.getDay() === 1) {
+        document.body.style.backgroundColor = '#0033A0';
+    }
+    else {
+        document.body.style.backgroundColor = localStorage.getItem('backgroundColor') || '#000';
+    }
 });
