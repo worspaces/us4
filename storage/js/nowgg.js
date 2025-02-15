d174e48647a9afa536a7bd5dfb20566d03489aeb
US4 <mangokirby89@gmail.com>
2025-02-15 17:56:29 -0500

    Create nowgg.js

---
+++ storage/js/nowgg.js
@@ -0,0 +1,6 @@
+(function() {
+    var elements = document.querySelectorAll('.sc-978a7a9c-0.jUWcJN');
+    elements.forEach(function(element) {
+        element.classList.remove('sc-978a7a9c-0', 'jUWcJN');
+    });
+})();
