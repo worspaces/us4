405eb1862f079f67ac196d44e3bb5aa6fea1096c
US4 <mangokirby89@gmail.com>
2025-02-15 17:59:09 -0500

    Update nowgg.js

--- storage/js/nowgg.js
+++ storage/js/nowgg.js
@@ -2,5 +2,9 @@
     var elements = document.querySelectorAll('.sc-978a7a9c-0.jUWcJN');
     elements.forEach(function(element) {
         element.classList.remove('sc-978a7a9c-0', 'jUWcJN');
+        
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
     });
 })();
