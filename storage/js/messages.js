27829d34d04e5ea7b79b24644e964a0999cadf69
US4 <mangokirby89@gmail.com>
2025-03-02 17:26:09 -0500

    Update messages.js

--- storage/js/messages.js
+++ storage/js/messages.js
@@ -1,16 +1,20 @@
-        const phrases = [
-                "hi guys",
-                "cheese",
-                { type: "image", src: "https://i.ibb.co/v6xfHBB5/7th-graders.png" },
-        
-        ];
+const phrases = [
+    "hi guys",
+    "cheese",
+    { type: "image", src: "https://i.ibb.co/v6xfHBB5/7th-graders.png" }
+];
 
-        const paragraph = document.getElementById('dynamicParagraph');
+const paragraph = document.getElementById('dynamicParagraph');
 
-        function changeText() {
-            const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-            paragraph.textContent = randomPhrase;
-        }
+function changeText() {
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
 
-        paragraph.addEventListener('click', changeText);
-        window.onload = changeText;
+    if (typeof randomPhrase === "string") {
+        paragraph.textContent = randomPhrase;
+    } else if (randomPhrase.type === "image") {
+        paragraph.innerHTML = `<img src="${randomPhrase.src}" alt="Splash Image" style="max-width: 100%;">`;
+    }
+}
+
+paragraph.addEventListener('click', changeText);
+window.onload = changeText;
