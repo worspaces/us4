4263ed11a1d3d65061af9d41287907fa4ffda871
US4 <mangokirby89@gmail.com>
2025-03-10 12:44:44 -0400

    Update settings-1.js

--- storage/js/settings-1.js
+++ storage/js/settings-1.js
@@ -24,7 +24,7 @@ const panicUrl = localStorage.getItem('panicUrl');
 if (panicKey && panicUrl) {
     window.addEventListener('keydown', function (event) {
         if (event.key === panicKey) {
-            window.location.href = panicUrl;
+            top.location.href = panicUrl;
         }
     });
 }
