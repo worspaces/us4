0ba46ae753381de8692be9c532d3ffbbf4166c57
US4 <mangokirby89@gmail.com>
2025-03-19 16:33:45 -0400

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -1,35 +1,20 @@
-    "use strict";
-    let destination = "";
+"use strict";
+let destination = "";
 
-    try {
-        destination = new URL(location.hash.slice(1)).toString();
-    } catch (err) {
-        alert(`Bad # string or bad URL. Got error:\n${err}`);
-        throw err;
-    }
+try {
+  destination = new URL(location.hash.slice(1)).toString();
+} catch (err) {
+  alert(`Bad # string or bad URL. Got error:\n${err}`);
+  throw err;
+}
 
-    const screenEffect = document.createElement("div");
-    screenEffect.id = "screenEffect";
-    document.body.appendChild(screenEffect);
-
-    setTimeout(() => {
-        screenEffect.classList.add("expand");
-
-        setTimeout(() => {
-            registerSW()
-                .then(() => {
-                    const newWindow = window.open(
-                        __uv$config.prefix + __uv$config.encodeUrl(destination),
-                        "_self"
-                    );
-
-                    newWindow.onload = () => {
-                        screenEffect.classList.add("split");
-                        setTimeout(() => screenEffect.remove(), 500);
-                    };
-                })
-                .catch((err) => {
-                    alert(`Encountered error:\n${err}`);
-                });
-        }, 500);
-    }, 200);
+registerSW()
+  .then(() => {
+    window.open(
+      __uv$config.prefix + __uv$config.encodeUrl(destination),
+      "_self"
+    );
+  })
+  .catch((err) => {
+    alert(`Encountered error:\n${err}`);
+  });
