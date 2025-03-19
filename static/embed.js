35b469da6cdcf623aed0623c28fba15455b7e72c
US4 <mangokirby89@gmail.com>
2025-03-19 16:24:43 -0400

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -1,20 +1,30 @@
-"use strict";
-let destination = "";
+    "use strict";
+    let destination = "";
 
-try {
-  destination = new URL(location.hash.slice(1)).toString();
-} catch (err) {
-  alert(`Bad # string or bad URL. Got error:\n${err}`);
-  throw err;
-}
+    try {
+        destination = new URL(location.hash.slice(1)).toString();
+    } catch (err) {
+        alert(`Bad # string or bad URL. Got error:\n${err}`);
+        throw err;
+    }
 
-registerSW()
-  .then(() => {
-    window.open(
-      __uv$config.prefix + __uv$config.encodeUrl(destination),
-      "_self"
-    );
-  })
-  .catch((err) => {
-    alert(`Encountered error:\n${err}`);
-  });
+    const openingScreen = document.createElement("div");
+    openingScreen.id = "openingScreen";
+    document.body.appendChild(openingScreen);
+
+    setTimeout(() => {
+        openingScreen.classList.add("shrink");
+
+        setTimeout(() => {
+            registerSW()
+                .then(() => {
+                    window.open(
+                        __uv$config.prefix + __uv$config.encodeUrl(destination),
+                        "_self"
+                    );
+                })
+                .catch((err) => {
+                    alert(`Encountered error:\n${err}`);
+                });
+        }, 800);
+    }, 200);
