6a27ea63c2466d6d16d89af08c8af864115a9510
US4 <mangokirby89@gmail.com>
2025-03-21 10:18:24 -0400

    Update splash-messages.js

--- storage/js/splash-messages.js
+++ storage/js/splash-messages.js
@@ -1,10 +1,14 @@
 const phrases = [
-    'hi guys',
-    'cheese',
-    { type: "image", src: 'https://i.ibb.co/v6xfHBB5/7th-graders.png' },
-    'dont type this on your keyboard: up, up, down, down, left, right, left, right, B, A, enter',
-    'emerity said you should watch bleach',
-    'im going to put bleach in your eyes'
+    `hi guys`,
+    `cheese`,
+    { type: "image", src: `https://i.ibb.co/v6xfHBB5/7th-graders.png` },
+    `dont type this on your keyboard: up, up, down, down, left, right, left, right, B, A, enter`,
+    `emerity said you should watch bleach`,
+    `im going to put bleach in your eyes`,
+    `shoutout to @literalaj on yt`,
+    `im in your walls`,
+    `"play the strongest battlegrounds" - literalaj`,
+    { type: "video", src: `https://media.tenor.com/SIpmtvnEsDIAAAPo/rotating-chips.mp4` },
 ];
 
 const paragraph = document.getElementById('dynamicParagraph');
@@ -16,6 +20,8 @@ function changeText() {
         paragraph.textContent = randomPhrase;
     } else if (randomPhrase.type === "image") {
         paragraph.innerHTML = `<img src="${randomPhrase.src}" alt="Splash Image" style="max-width: 100%;">`;
+    } else if (randomPhrase.type === "video") {
+        paragraph.innerHTML = `<video controls style="max-width: 100%; height: auto;"> <source src="${randomPhrase.src}" type="video/mp4"> </video>`;
     }
 }
 
