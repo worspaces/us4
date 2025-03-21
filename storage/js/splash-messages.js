69ceef954d232cf7b7ce896c270ea8cdb9ebf46c
US4 <mangokirby89@gmail.com>
2025-03-21 10:32:16 -0400

    Update splash-messages.js

--- storage/js/splash-messages.js
+++ storage/js/splash-messages.js
@@ -22,7 +22,7 @@ function changeText() {
     } else if (randomPhrase.type === "image") {
         paragraph.innerHTML = `<img src="${randomPhrase.src}" alt="Splash Image" style="max-width: 100%;">`;
     } else if (randomPhrase.type === "video") {
-        paragraph.innerHTML = `<video controls autoplay style="max-width: ${randomPhrase.width}; height: auto;"> <source src="${randomPhrase.src}" type="video/mp4"> </video>`;
+        paragraph.innerHTML = `<video controls loop autoplay style="max-width: ${randomPhrase.width}; height: auto;"> <source src="${randomPhrase.src}" type="video/mp4"> </video>`;
     }
 }
 
