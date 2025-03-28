f359388597a2b458d0d1dfe188516ed2e71269e5
US4 <mangokirby89@gmail.com>
2025-03-28 12:45:21 -0400

    Update splash-messages.js

--- storage/js/splash-messages.js
+++ storage/js/splash-messages.js
@@ -12,7 +12,7 @@ const phrases = [
     `jackson is batman`,
     `jacob is the WORST violin. Make sure to bully him (8th grade violin, probably 400 pounds tbh)`,
     `Jaxon I see U`,
-    { type: "image", width: '50%', src: `https://c.tenor.com/9MVlipGuNioAAAAC/tenor.gif` },
+    { type: "image", width: '400px', src: `https://c.tenor.com/9MVlipGuNioAAAAC/tenor.gif` },
 ];
 
 const paragraph = document.getElementById('dynamicParagraph');
